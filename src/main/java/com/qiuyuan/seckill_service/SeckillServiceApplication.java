b5b66e7ee4de55f23fca88ac1b6d80e55bdc07d7
qiuyuan1111 <744596674@qq.com>
2026-02-03 01:09:25 +0800

    fix: start app without datasource

--- src/main/java/com/qiuyuan/seckill_service/SeckillServiceApplication.java
+++ src/main/java/com/qiuyuan/seckill_service/SeckillServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeckillServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SeckillServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SeckillServiceApplication.class, args);
+  }
 }
